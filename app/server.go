3579681549993044d7258af6fb9cd94229bec5ba
vikramschauhan <vikram.rana@live.com>
2024-05-01 22:00:22 +0530

    Handle multiple requests

--- app/server.go
+++ app/server.go
@@ -11,35 +11,38 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	go func() {
-		l, err := net.Listen("tcp", "0.0.0.0:4221")
-		if err != nil {
-			fmt.Println("Failed to bind to port 4221")
-			os.Exit(1)
-		}
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection : ", err.Error())
 			os.Exit(1)
 		}
-		requestData := make([]byte, 1024)
-		_, err = conn.Read(requestData)
-		if err != nil {
-			fmt.Println("Error while reading from the connection:", err.Error())
-		}
-		requestString := string(requestData)
-		httpMethod := strings.Split(requestString, "\r\n")[0]
-		pathString := strings.Split(httpMethod, " ")[1]
-		if pathString == "/" {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		} else if strings.HasPrefix(pathString, "/echo/") {
-			content := strings.TrimSpace(pathString[6:])
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length:" + strconv.Itoa(len(content)) + "\r\n\r\n" + content))
-		} else if strings.HasPrefix(pathString, "/user-agent") {
-			userAgent := strings.Split(requestString, "\r\n")[2]
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length:" + strconv.Itoa(len(userAgent[12:])) + "\r\n\r\n" + userAgent[12:]))
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		}
-	}()
+		go handleRequest(conn)
+	}
+}
+func handleRequest(conn net.Conn) {
+	requestData := make([]byte, 1024)
+	_, err := conn.Read(requestData)
+	if err != nil {
+		fmt.Println("Error while reading from the connection:", err.Error())
+	}
+	requestString := string(requestData)
+	httpMethod := strings.Split(requestString, "\r\n")[0]
+	pathString := strings.Split(httpMethod, " ")[1]
+	if pathString == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.HasPrefix(pathString, "/echo/") {
+		content := strings.TrimSpace(pathString[6:])
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length:" + strconv.Itoa(len(content)) + "\r\n\r\n" + content))
+	} else if strings.HasPrefix(pathString, "/user-agent") {
+		userAgent := strings.Split(requestString, "\r\n")[2]
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length:" + strconv.Itoa(len(userAgent[12:])) + "\r\n\r\n" + userAgent[12:]))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
